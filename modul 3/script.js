709710f9b4dba9b483951c585392117080d78862
Anna <anna.zarova.2012@gmail.com>
2025-04-06 23:24:03 +0300

    рп

--- modul 3/script.js
+++ modul 3/script.js
@@ -1,44 +1,78 @@
-let user = {name: "Аня", age: 12, admin: true}
+// let user = {name: "Аня", age: 12, admin: true}
 
-console.log(user)
+// console.log(user)
 
-console.log(user.admin)
+// console.log(user.admin)
 
-console.log(user.country)
+// console.log(user.country)
 
-user.country = "Latvia"
+// user.country = "Latvia"
 
-console.log(user.country)
+// console.log(user.country)
 
-let games = ["fnaf", "Roblox", "Hello Neighbor"]
+// let games = ["fnaf", "Roblox", "Hello Neighbor"]
 
-console.log(games[2])
+// console.log(games[2])
 
 
 
-console.log(5 + 5); 
-console.log(-6 + 5);
-console.log(-6 - 12);
-console.log(6 / 99);
-console.log(77 % 7);
-console.log(4 ** 5);
-console.log(12 * 16);
+// console.log(5 + 5); 
+// console.log(-6 + 5);
+// console.log(-6 - 12);
+// console.log(6 / 99);
+// console.log(77 % 7);
+// console.log(4 ** 5);
+// console.log(12 * 16);
 
-console.log(1 / 0);
+// console.log(1 / 0);
 
-console.log('это не число' / 12);
+// console.log('это не число' / 12);
 
-const number = 42;
-const negativeNumber = -15;
-const bigNumber = 999999999999999;
-const text = "Привет мир";
-const isTrue = true;
-const isFalse = false;
-const emptyvalue = null;
-let undefinedValue;
+// const number = 42;
+// const negativeNumber = -15;
+// const bigNumber = 999999999999999n;
+// const text = "Привет мир";
+// const isTrue = true;
+// const isFalse = false;
+// const emptyvalue = null;
+// let undefinedValue;
 
-const obj = {age: 25,name: "Алекс",isStudent: false};
+// const obj = {age: 25,name: "Алекс",isStudent: false};
 
-const array = [1, "Текст", true, null];
+// const array = [1, "Текст", true, null];
 
-console.log({ number, negativeNumber, bigNumber, text, isTrue, isFalse, emptyvSalue, undefinedValue, obj, array });
+// console.log({ number, negativeNumber, bigNumber, text, isTrue, isFalse, emptyvalue, undefinedValue, obj, array });
+
+let user = prompt("Введи свой возраст")
+
+// if(user === "pass981"){
+//     alert("Вход выполнен!")
+// }
+
+// else{
+//     alert("Не успех.")
+// }
+
+// if(user === "pass981"){
+//     alert("Вход выполнен!")
+// }
+
+// else if(user === "tool81"){
+//     alert("Выполнен вход с ограничениями!")
+// }
+
+// else{
+//     alert("Нет доступа!")
+// }
+
+if(user < 18){
+    alert("Привет, как дела?")
+}
+
+else if(user <= 35){
+    alert("Добрый день")
+}
+
+else if(user > 35){
+    alert("Доброго времени суток")
+}
